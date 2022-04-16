622a4eda2e3c02a50cd346f3c00734da5f492a58
Oscar Lima <oscar.lima@dfki.de>
2022-04-16 21:38:08 +0200

    remove extra code from motor speed control example

--- firmware/examples/motor_speed_control/motor_speed_control.ino
+++ firmware/examples/motor_speed_control/motor_speed_control.ino
@@ -14,7 +14,7 @@
  */
 
 #include <PinChangeInt.h>
-#include <PID_v1.h>                  // PID based control library to achieve motor speed setpoint as fast and smooth as possible
+#include <PID_v1.h> // PID based control library to achieve motor speed setpoint as fast and smooth as possible
 
 /******** DEFINE *********/
 
@@ -32,31 +32,10 @@
 #define IN2_MRR 10
 #define PWM_MRR 9   // ENA: enable/disable H bridge (used as PWM to control motor speed)
 
-#define IN1_MRL 12  // MRL: Motor rear left
-#define IN2_MRL 13
-#define PWM_MRL 11
-
-#define IN1_MFR 7   // MFR: Motor front right
-#define IN2_MFR 5
-#define PWM_MFR 6
-
-#define IN1_MFL 4   // MFL: Motor front left
-#define IN2_MFL 2
-#define PWM_MFL 3
-
-// encoders
+// encoder
 
 #define hall_sensor_RR1 A14 // RR
 #define hall_sensor_RR2 A15 // RR
-
-#define hall_sensor_RL1 A13 // RL
-#define hall_sensor_RL2 A12 // RL
-
-#define hall_sensor_FR1 A9  // FR
-#define hall_sensor_FR2 A11 // FR
-
-#define hall_sensor_FL1 A8  // FL
-#define hall_sensor_FL2 A10 // FL
 
 #define pulses_per_revolution 450
 
@@ -67,24 +46,15 @@
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
 const int motor_offset_rr = 1;
-const int motor_offset_rl = 1;
-const int motor_offset_fr = 1;
-const int motor_offset_fl = 1;
 
 // Initializing motors.  The library will allow you to initialize as many
 // motors as you have memory for.  If you are using functions like forward
 // that take 2 motors as arguements you can either write new functions or
 // call the function more than once.
 Motor motor_rr = Motor(IN1_MRR, IN2_MRR, PWM_MRR, motor_offset_rr);
-Motor motor_rl = Motor(IN1_MRL, IN2_MRL, PWM_MRL, motor_offset_rl);
-Motor motor_fr = Motor(IN1_MFR, IN2_MFR, PWM_MFR, motor_offset_fr);
-Motor motor_fl = Motor(IN1_MFL, IN2_MFL, PWM_MFL, motor_offset_fl);
 
 // encoder tick counters
 volatile unsigned int count_rr = 0;
-volatile unsigned int count_rl = 0;
-volatile unsigned int count_fr = 0;
-volatile unsigned int count_fl = 0;
 
 // measure elapsed time
 unsigned long last_time;
@@ -112,11 +82,8 @@ void setup()
 {
     // encoder pin configuration
     pinMode(hall_sensor_RR1, INPUT);
-    pinMode(hall_sensor_RL1, INPUT);
     digitalWrite(hall_sensor_RR1, HIGH); //use the internal pullup resistor
-    digitalWrite(hall_sensor_RL1, HIGH); //use the internal pullup resistor
     PCintPort::attachInterrupt(hall_sensor_RR1, interruptCountRR, CHANGE);
-    PCintPort::attachInterrupt(hall_sensor_RL1, interruptCountRL, CHANGE);
 
     // initialize serial port at 9600 baud rate
     Serial.begin(9600);
@@ -144,21 +111,6 @@ void interruptCountRR()
 {
     count_rr++;
 }
-
-void interruptCountRL()
-{
-    count_rl++;
-}
-
-//void interruptCountFR()
-//{
-//    count_fr++;
-//}
-//
-//void interruptCountFL()
-//{
-//    count_fl++;
-//}
 
 float measureSpeed()
 {
